762509a7c313ba014f958c6798c291b27cc57737
Jan Prikryl <prikryl@fd.cvut.cz>
2016-05-02 15:33:29 +0200

    New Smarty plugin for typesetting root factors, used for typesetting rational polynomial functions using MathML.

---
+++ classes/plugins/function.rootfactor.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Typeset rational function root factor.
+ * The root factor is (x+x_infty) or x.
+ */
+function smarty_function_rootfactor($params, &$smarty)
+{
+    /* Get the variable name, i.e. "p" or "s" or "z". */
+    $var = $params['var'];
+
+    /* Check that it is not empty. */
+    if (empty($var))
+    {
+        $smarty->trigger_error("rootfactor: missing 'var' parameter");
+        return NULL;
+    }
+
+    /* In case that the value of root is equal to zero, we will not typeset parentheses around the variable. */
+    if (empty($params['re']) && empty($params['im']))
+    {
+        $par_open = $par_close = '';
+    }
+    else
+    {
+        $par_open = '(';
+        $par_close = ')';
+    }
+
+    return $par_open . $var . $params['re'] . $params['im'] . $par_close;
+}
+
+?>
